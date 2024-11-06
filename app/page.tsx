cc1aec5a928ff3b6142b955e625cd7e8e64da1c0
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-11-06 23:47:26 +0530

    build : prisma orm  and next-auth added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+"use client";
+
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full h-dvh flex justify-center items-center">
+      <div className="flex justify-center items-center flex-col">
+        <motion.h1
+          initial={{
+            opacity: 0,
+            scale: 0.95,
+            filter: "blur(10px)",
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 0.35 }}
+          className="text-4xl sm:text-6xl font-bold"
+        >
+          Visiogen
+        </motion.h1>
+        <motion.p
+          initial={{
+            opacity: 0,
+            scale: 0.95,
+            filter: "blur(10px)",
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 0.35, delay: 0.35 }}
+          className="text-centre text-white/50 mt-2"
+        >
+          Generate stunning images from text using AI models for free
+        </motion.p>
+      </div>
     </div>
-  )
+  );
 }
