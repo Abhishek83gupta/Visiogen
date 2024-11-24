cad91fa1af1cebf396928546d5632df46bc06958
Abhishek83gupta <abhi12345678gupta@gmail.com>
2024-11-24 18:38:29 +0530

    style : fix some css class

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="w-full h-dvh flex justify-center items-center">
-      <div className="flex justify-center items-center flex-col">
+      <div className="flex justify-center items-center flex-col text-center">
         <motion.h1
           initial={{
             opacity: 0,
@@ -19,7 +19,7 @@ export default function Home() {
             scale: 1,
             filter: "blur(0px)",
           }}
-          transition={{ duration: 0.35, delay: 0}}
+          transition={{ duration: 0.35, delay: 0 }}
           className="text-4xl sm:text-6xl font-bold"
         >
           Visiogen
@@ -36,22 +36,23 @@ export default function Home() {
             filter: "blur(0px)",
           }}
           transition={{ duration: 0.35, delay: 0.35 }}
-          className="text-centre text-white/50 mt-2"
+          className="text-center text-white/50 mt-2 px-5 sm:px-10"
         >
-          Generate stunning images from text using AI models for free
+          Generate stunning images from text using AI models
+           for free
         </motion.p>
         <motion.div
-        initial={{
-          opacity: 0,
-          scale: 0.95,
-          filter: "blur(10px)",
-        }}
-        animate={{
-          opacity: 1,
-          scale: 1,
-          filter: "blur(0px)",
-        }}
-        transition={{ duration: 0.35, delay: 0.7 }}
+          initial={{
+            opacity: 0,
+            scale: 0.95,
+            filter: "blur(10px)",
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 0.35, delay: 0.7 }}
         >
           <Link href="/create">
             <Button className="mt-3 font-bold p-5">Start creating</Button>
